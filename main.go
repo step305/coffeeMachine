cb5541f120771fa02f133ed82f7d8fc6aaeef65f
step305 <step305@mail.ru>
2022-08-13 11:54:51 +0300

    1.0.0

--- main.go
+++ main.go
@@ -12,8 +12,9 @@ const infoCoffeeMachineState string = "The coffee machine has:\n" +
 	"%d disposable cups\n" +
 	"$%d of money\n"
 
-const requestActionChoice string = "Write action (buy, fill, take):\n"
-const requestCoffeeTypeChoice string = "What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:\n"
+const requestActionChoice string = "Write action (buy, fill, take, remaining, exit):\n"
+const requestCoffeeTypeChoice string = "What do you want to buy? 1 - espresso, " +
+	"2 - latte, 3 - cappuccino, 4 - marocchino:\n"
 const requestAmountWaterAdd string = "Write how many ml of water you want to add:\n"
 const requestAmountMilkAdd string = "Write how many ml of milk you want to add:\n"
 const requestAmountCoffeeBeansAdd string = "Write how many grams of coffee beans you want to add:\n"
@@ -23,22 +24,23 @@ const responseOnTake string = "I gave you $%d\n"
 const errorInvalidInput string = "[Error] invalid input"
 const errorUnknownCommand string = "[Error] unknown command"
 const errorInvalidCoffeeType string = "[Error] invalid coffee type chosen"
-const errorDivisionByZero string = "[Error] division by zero"
-const errorNotEnoughIngredients string = "No, I can make only %d cups of coffee"
+const errorNotEnoughIngredients string = "[Error] out of ingredients"
+
+var notSoErrorExit = errors.New("exiting")
 
 type recipe struct {
-	milk  int
-	water int
-	beans int
-	cost  int
+	milk  uint
+	water uint
+	beans uint
+	cost  uint
 }
 
 type coffeeMachine struct {
-	milk  int
-	water int
-	beans int
-	cups  int
-	money int
+	milk  uint
+	water uint
+	beans uint
+	cups  uint
+	money uint
 }
 
 type action func(machine *coffeeMachine) error
@@ -64,29 +66,96 @@ var cappuccinoRecipe = recipe{
 	cost:  6,
 }
 
+var marocchinoRecipe = recipe{
+	milk:  25,
+	water: 250,
+	beans: 16,
+	cost:  8,
+}
+
+var knownRecipes = []recipe{
+	espressoRecipe,
+	latteRecipe,
+	cappuccinoRecipe,
+	marocchinoRecipe,
+}
+
 var knownCommands = map[string]action{
-	"buy":  (*coffeeMachine).buy,
-	"fill": (*coffeeMachine).fill,
-	"take": (*coffeeMachine).take,
+	"buy":       (*coffeeMachine).buy,
+	"fill":      (*coffeeMachine).fill,
+	"take":      (*coffeeMachine).take,
+	"remaining": (*coffeeMachine).state,
+	"exit":      (*coffeeMachine).exit,
+}
+
+func (s *coffeeMachine) exit() error {
+	return notSoErrorExit
 }
 
 func (s *coffeeMachine) fill() error {
-	fmt.Println("I am fill")
+	var water, milk, beans, cups uint
+	err := requestUserInput(requestAmountWaterAdd, &water)
+	if err != nil {
+		return err
+	}
+	err = requestUserInput(requestAmountMilkAdd, &milk)
+	if err != nil {
+		return err
+	}
+	err = requestUserInput(requestAmountCoffeeBeansAdd, &beans)
+	if err != nil {
+		return err
+	}
+	err = requestUserInput(requestAmountCupsAdd, &cups)
+	if err != nil {
+		return err
+	}
+	s.water += water
+	s.milk += milk
+	s.beans += beans
+	s.cups += cups
 	return nil
 }
 
 func (s *coffeeMachine) take() error {
-	fmt.Println("I am take")
+	fmt.Printf(responseOnTake, s.money)
+	s.money = 0
 	return nil
+}
+
+func (s *coffeeMachine) validateBuy(coffee recipe) bool {
+	if s.water >= coffee.water && s.milk >= coffee.milk && s.beans >= coffee.beans && s.cups > 0 {
+		return true
+	}
+	return false
 }
 
 func (s *coffeeMachine) buy() error {
-	fmt.Println("I am buy")
+	var coffeeType int
+	err := requestUserInput(requestCoffeeTypeChoice, &coffeeType)
+	if err != nil {
+		return err
+	}
+
+	if coffeeType < 1 || coffeeType > len(knownRecipes) {
+		return errors.New(errorInvalidCoffeeType)
+	}
+	coffeeType--
+	if s.validateBuy(knownRecipes[coffeeType]) {
+		s.water -= knownRecipes[coffeeType].water
+		s.milk -= knownRecipes[coffeeType].milk
+		s.beans -= knownRecipes[coffeeType].beans
+		s.cups--
+		s.money += knownRecipes[coffeeType].cost
+	} else {
+		return errors.New(errorNotEnoughIngredients)
+	}
 	return nil
 }
 
-func (s *coffeeMachine) state() string {
-	return fmt.Sprintf(infoCoffeeMachineState, s.water, s.milk, s.beans, s.cups, s.money)
+func (s *coffeeMachine) state() error {
+	fmt.Printf(infoCoffeeMachineState, s.water, s.milk, s.beans, s.cups, s.money)
+	return nil
 }
 
 func (s *coffeeMachine) request() (string, error) {
@@ -116,22 +185,25 @@ func main() {
 		money: 550,
 	}
 
-	fmt.Print(machine.state())
-	command, err := machine.request()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	f, exist := knownCommands[command]
-	if exist {
-		err := f(&machine)
+	for {
+		command, err := machine.request()
 		if err != nil {
 			fmt.Println(err)
-			return
+			continue
 		}
-	} else {
-		fmt.Println(errorUnknownCommand)
-		return
+		f, exist := knownCommands[command]
+		if exist {
+			err := f(&machine)
+			if err != nil {
+				if errors.Is(err, notSoErrorExit) {
+					return
+				}
+				fmt.Println(err)
+				continue
+			}
+		} else {
+			fmt.Println(errorUnknownCommand)
+			continue
+		}
 	}
-	fmt.Print(machine.state())
 }
